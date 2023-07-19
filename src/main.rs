5cfd3fd0aee889e041df2930e771bec0be6afcbf
sbeylot <sbeylot@student.42.fr>
2023-07-19 23:09:14 +0200

    Adding Philo and routine

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,139 @@
 
 mod parsing;
 
-use std::env;
+use std::{
+    env,
+    time::{Duration, Instant},
+    thread,
+};
+
+use std::sync::{
+    Arc, 
+    Mutex
+};
 
 use parsing::Data;
 
+#[derive(Debug)]
+struct Philosopher {
+    id: usize,
+    left: usize,
+    right: usize,
+    start_time: Instant,
+    time_to_die: u128,
+    table: Arc<Data>,
+}
+
+enum Speak {
+    Dead,
+    Think,
+    Eat,
+    Sleep,
+    Done,
+    Fork,
+}
+
+impl Philosopher {
+    fn new(id: usize, left: usize, right: usize, time_to_die: u128, table: Arc<Data>) -> Philosopher {
+        Philosopher { 
+            id: id, 
+            left: left, 
+            right: right, 
+            start_time: Instant::now(), 
+            time_to_die: time_to_die, 
+            table: table }
+    }
+
+    fn run(&mut self) {
+        'dead: loop {
+            if !self.is_alive() {
+                break 'dead;
+            }
+            self.speak(Speak::Think);
+
+
+            let left = self.table.forks[self.left].lock().unwrap();
+            self.speak(Speak::Fork);
+            let right = self.table.forks[self.right].lock().unwrap();
+            self.speak(Speak::Fork);
+
+
+            if !self.is_alive() {
+                break 'dead;
+            } else {
+                self.time_to_die += self.start_time.elapsed().as_millis();
+            }
+            self.speak(Speak::Eat);
+
+            thread::sleep(Duration::from_millis(self.table.time_to_eat));
+            if !self.is_alive() {
+                break 'dead;
+            }
+            drop(left);
+            drop(right);
+
+            if !self.is_alive() {
+                break 'dead;
+            }
+            self.speak(Speak::Sleep);
+            thread::sleep(Duration::from_millis(self.table.time_to_sleep));
+        }
+    }
+
+    fn speak(&self, sentence: Speak) {
+        let time = self.start_time.elapsed().as_millis();
+        match sentence {
+            Speak::Done => println!("[{}ms] {} is done eating!", time, self.id),
+            Speak::Think => println!("[{}ms] {} is thinking!", time, self.id),
+            Speak::Eat => println!("[{}ms] {} is eating!", time, self.id),
+            Speak::Sleep => println!("[{}ms] {} is sleeping!", time, self.id),
+            Speak::Fork => println!("[{}ms] {} has taken a fork!", time, self.id),
+            _ => {}
+        }
+    }
+
+    fn is_alive(&self) -> bool {
+        let time = self.start_time.elapsed().as_millis();
+        if time > self.time_to_die {
+            println!("[{}ms] {} died", self.time_to_die, self.id);
+            return false;
+        }
+        true
+    }
+
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let data: Data = match Data::build(args) {
-        Ok(info) => match info.check_value() {
-            Ok(()) => info,
-            Err(err) => {
-                println!("Error {err}");
-                std::process::exit(1);
-            }
-        },
-        Err(err) => {println!("Error {err}"); std::process::exit(1)},
+        Ok(info) => info,
+        Err(err) => {
+            eprintln!("{err}");
+            std::process::exit(1);
+        }
     };
+    let nbr_philo = data.nbr_philo;
+    let time_to_die = data.time_to_die;
+    let table: Arc<Data> = Arc::new(data);
 
-    println!("Data: {:?}", data);
+    let philosophers: Vec<Philosopher> = (0..nbr_philo as usize).into_iter().map(|i| {
+        let left = i;
+        let right = (i + 1) % nbr_philo as usize;
+        Philosopher::new(i, left, right, time_to_die, Arc::clone(&table))
+    }).collect();
+
+    let handles: Vec<_> = philosophers
+        .into_iter()
+        .map(|mut philo| {
+            thread::spawn(move || {
+                philo.run();
+            })
+        })
+    .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
 }
--- src/parsing.rs
+++ src/parsing.rs
@@ -1,20 +1,31 @@
+use std::sync::Mutex;
+
 #[derive(Debug)]
 pub struct Data {
     pub nbr_philo: u32,
-    pub time_to_die: u32,
-    pub time_to_eat: u32,
-    pub time_to_sleep: u32,
+    pub time_to_die: u128,
+    pub time_to_eat: u64,
+    pub time_to_sleep: u64,
     pub nbr_meal: u32,
     meal_enable: bool,
+    pub forks: Vec<Mutex<()>>,
 }
 
 impl Data {
-    fn new() -> Self {
-        Data { nbr_philo: 0, time_to_die: 0, time_to_eat: 0, time_to_sleep: 0, nbr_meal: 0, meal_enable: false }
+    pub fn new() -> Self {
+        Data { 
+            nbr_philo: 0,
+            time_to_die: 0,
+            time_to_eat: 0,
+            time_to_sleep: 0,
+            nbr_meal: 0,
+            meal_enable: false,
+            forks: Vec::new(),
+        }
     }
 
     pub fn build(args: Vec<String>) -> Result<Data, String> {
-        if args.len() < 5 && args.len() > 6 {
+        if args.len() != 5 && args.len() != 6 {
             eprintln!("Error: Wrong number of argument!\nusage: cargo r -- nbr_philo time_to_die time_to_eat time_to_sleep [nbr_meal]");
             std::process::exit(1);
         }
@@ -30,9 +41,9 @@ impl Data {
                 Ok(value) => {
                     match i {
                         0 => data.nbr_philo = value,
-                        1 => data.time_to_die = value,
-                        2 => data.time_to_eat = value,
-                        3 => data.time_to_sleep = value,
+                        1 => data.time_to_die = value as u128,
+                        2 => data.time_to_eat = value as u64,
+                        3 => data.time_to_sleep = value as u64,
                         4 => data.nbr_meal = value,
                         _ => continue
                     }
@@ -43,6 +54,18 @@ impl Data {
                 }
             }
         }
+        match data.check_value() {
+            Ok(info) => {},
+            Err(err) => {
+                return Err(err);
+            }
+        };
+
+        for _ in 0..data.nbr_philo {
+            let fork = Mutex::new(());
+            data.forks.push(fork);
+        }
+
         Ok(data)
     }
 
